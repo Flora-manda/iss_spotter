11e0f252479d15f3d8f3aaa0a00cffc96db2a068
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-15 16:38:24 +0000

    refactored code using promise

---
+++ index2.js
@@ -0,0 +1,27 @@
+// index2.js
+
+const { fetchMyIP } = require('./iss_promised');
+const { fetchCoordsByIP } = require('./iss_promised');
+const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+const { printPassTimes } = require('./index.js');
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+
+
+// see index.js for printPassTimes 
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call 
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+  
